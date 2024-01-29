76f9c50708d99b4ad3a43c9a47d91cdc268f364b
Fernando Herrera <herrera770@outlook.es>
2024-01-29 17:02:20 -0300

    se agregaron expresiones regulares, states de cada elemento de formulario y la funcion necesaria para verificar los datos del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,24 +1,79 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const Formulario = () => {
+  // estados de cada input del form
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [dni, setDNI] = useState("");
+  const [correo, setCorreo] = useState("");
+  // expresiones regulares
+
+  const expRegNombre = /^[a-zA-ZÀ-ÿ]{3,20}$/;
+  const expRegApellido = /^[a-zA-ZÀ-ÿ]{3,25}$/;
+  const expRegDNI = /^[0-9]{8,8}$/;
+  const expRegCorreo =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    //Funciones del formulario
+  function verificarFormulario(e) {
+    e.preventDefault();
+    if(expRegNombre.test(nombre) && expRegApellido.test(apellido) && expRegDNI.test(dni) && expRegCorreo.test(correo)){
+        alert("Datos enviados correctamente")
+    }else{
+        alert("Por favor, verifique que los datos concuerden con lo solicitado")
+    }
+  }
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={verificarFormulario}>
         <Form.Group className="mb-3" controlId="nombre">
-            <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" placeholder="Introduzca su nombre" minLength={3} maxLength={20} required/>
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Introduzca su nombre"
+            minLength={3}
+            maxLength={20}
+            required
+            onChange={(e) => setNombre(e.target.value)}
+            value={nombre}
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="apellido"> 
-            <Form.Label>Apellido</Form.Label>
-            <Form.Control type="text" placeholder="Introduzca su apellido" minLength={3} maxLength={25} required />
+        <Form.Group className="mb-3" controlId="apellido">
+          <Form.Label>Apellido</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Introduzca su apellido"
+            minLength={3}
+            maxLength={25}
+            onChange={(e) => setApellido(e.target.value)}
+            value={apellido}
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="dni">
-            <Form.Label>DNI</Form.Label>
-            <Form.Control type="number" placeholder="Introduzca su número de DNI" min={11111111} max={99999999} required></Form.Control>
+          <Form.Label>DNI</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Introduzca su número de DNI"
+            min={11111111}
+            max={99999999}
+            onChange={(e) => setDNI(e.target.value)}
+            value={dni}
+            required
+          ></Form.Control>
         </Form.Group>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Correo electrónico</Form.Label>
-          <Form.Control type="email" placeholder="introduzca su corro electrónico" min={10} max={50} required/>
+          <Form.Control
+            type="email"
+            placeholder="introduzca su corro electrónico"
+            min={10}
+            max={50}
+            onChange={(e) => setCorreo(e.target.value)}
+            value={correo}
+            required
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Enviar
