2226728d3a68ac67b1ee33a43dc3e86de49b32a0
Fernando Herrera <herrera770@outlook.es>
2024-01-29 13:32:11 -0300

    se modiicaron los requisitos para validar los diferentes campos del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,19 +6,19 @@ const Formulario = () => {
       <Form>
         <Form.Group className="mb-3" controlId="nombre">
             <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" placeholder="Introduzca su nombre" minLength={3} maxLength={20}/>
+            <Form.Control type="text" placeholder="Introduzca su nombre" minLength={3} maxLength={20} required/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="apellido"> 
             <Form.Label>Apellido</Form.Label>
-            <Form.Control type="text" placeholder="Introduzca su apellido" minLength={3} maxLength={25} />
+            <Form.Control type="text" placeholder="Introduzca su apellido" minLength={3} maxLength={25} required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="dni">
             <Form.Label>DNI</Form.Label>
-            <Form.Control type="number" placeholder="Introduzca su número de DNI" min={11111111} max={99999999}></Form.Control>
+            <Form.Control type="number" placeholder="Introduzca su número de DNI" min={11111111} max={99999999} required></Form.Control>
         </Form.Group>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Correo electrónico</Form.Label>
-          <Form.Control type="email" placeholder="introduzca su corro electrónico" />
+          <Form.Control type="email" placeholder="introduzca su corro electrónico" min={10} max={50} required/>
         </Form.Group>
         <Button variant="primary" type="submit">
           Enviar
